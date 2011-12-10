6ba1ef48f8b617d1b5948af6d56762716e856ac3
Pedro Gomes <pdcgomes@gmail.com>
2011-12-10 13:12:06 +0000

    Initial Commit

---
+++ Chainz/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Chainz
+//
+//  Created by Pedro Gomes on 12/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window_;
+	RootViewController	*viewController_;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
---
+++ Chainz/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Chainz
+//
+//  Created by Pedro Gomes on 12/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
